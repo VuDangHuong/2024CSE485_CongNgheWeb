da24cbbe61625051dd028b1c5d8f0b1e3ef54b99
VuDangHuong <danghuong30082003@gmail.com>
2024-12-07 02:47:27 +0700

    update code

---
+++ tlunews/index.php
@@ -0,0 +1,19 @@
+<?php
+$controller = $_GET['controller'] ?? 'Home';
+$action = $_GET['action'] ?? 'index';
+
+$controller = ucfirst($controller) . 'Controller';
+$controllerFile = "controllers/$controller.php";
+
+if (file_exists($controllerFile)) {
+    require_once $controllerFile;
+    $controllerObj = new $controller();
+    if (method_exists($controllerObj, $action)) {
+        $controllerObj->$action();
+    } else {
+        echo "Action không tồn tại.";
+    }
+} else {
+    echo "Controller không tồn tại.";
+}
+?>
